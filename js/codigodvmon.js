a5b56f238883722ed94dd0268fe4cda2a2c19a28
Davidjv11 <dsantijasso11@gmail.com>
2026-03-01 12:47:07 -0700

    Intentando arreglar los botones
    Haciendo que los botones de seleccionar devmon de pc y player se detecten en el HTML

--- js/codigodvmon.js
+++ js/codigodvmon.js
@@ -1,47 +1,106 @@
-function iniciarJuego() {
+//Botones para elegir devmon
+let spanDevmonPlayer
+
+
+function iniciarJuego( ){
+    spanDevmonPlayer = document.getElementById("DevmonPlayer")
+
     let botonSalmonoque = document.getElementById("botonSalmonoque")
-    botonSalmonoque.addEventListener("click",seleccionarSalmonoque)
+    botonSalmonoque.addEventListener("click", seleccionarSalmonoque)
 
     let botonDragonaitor = document.getElementById("botonDragonaitor")
-    botonDragonaitor.addEventListener("click",seleccionarSalmonoque)
+    botonDragonaitor.addEventListener("click", seleccionarDragonaitor)
 
 
     let botonTortugor = document.getElementById("botonTortugor")
-    botonTortugor.addEventListener("click",seleccionarTortugor)
+    botonTortugor.addEventListener("click", seleccionarTortugor)
 
     let botonTiburoncin = document.getElementById("botonTiburoncin")
-    botonTiburoncin.addEventListener("click",seleccionarTiburoncin)
+    botonTiburoncin.addEventListener("click", seleccionarTiburoncin)
 
     let botonPalmon = document.getElementById("botonPalmon")
-    botonPalmon.addEventListener("click",seleccionarPalmon)
+    botonPalmon.addEventListener("click", seleccionarPalmon)
 
     let botonLeñon = document.getElementById("botonLeñon")
-    botonLeñon.addEventListener("click",seleccionarLeñon)
+    botonLeñon.addEventListener("click", seleccionarLeñon)
 
-}
+ 
 
-function seleccionarSalmonoque() {
-    alert("Selecionaste a Salmonoque")
-}
-function seleccionarDragonaitor() {
-    alert("Selecionaste a Dragonaitor")
-}
-function seleccionarTortugor() {
-    alert("Selecionaste a Tortugor")
-}
+    }
+//alerta por seleccionar un devmon
 
-function seleccionarTiburoncin() {
-    alert("Selecionaste a Tiburoncin")
+
+    function seleccionarSalmonoque() {
+        alert("Selecionaste a Salmonoque")
+        spanDevmonPlayer.innerHTML = "Salmonoque" 
+    }
+    function seleccionarDragonaitor() {
+        alert("Selecionaste a Dragonaitor")
+        spanDevmonPlayer.innerHTML ="Dragonaitor"
+    }
+    function seleccionarTortugor() {
+        alert("Selecionaste a Tortugor")
+        spanDevmonPlayer.innerHTML = "Tortugor" 
+    }   
+
+    function seleccionarTiburoncin() {
+        alert("Selecionaste a Tiburoncin")
+        spanDevmonPlayer.innerHTML = "Tiburoncin" 
     }
 
-function seleccionarPalmon() {
-    alert("Selecionaste a Palmon")
+    function seleccionarPalmon() {
+        alert("Selecionaste a Palmon")
+        spanDevmonPlayer.innerHTML = "Palmon" 
     }
 
-function seleccionarLeñon() {
-    alert("Selecionaste a Leñon")
+    function seleccionarLeñon() {
+        alert("Selecionaste a Leñon")
+        spanDevmonPlayer.innerHTML = "Leñon" 
+    }   
+
+   
+
+
+//PC elige devmon aleatoriamente
+let selectDevmonPc
+
+    spanDevmonPc = document.getElementById("DevmonPc")
+
+
+function selectDevmonPc(){
+    let DevmonPc = ramdom(1,6)
+    let spanDevmonPc = document.getElementById("DevmonPc")
+
+    if (DevmonPc == 1 ){
+        spanDevmonPc.innerHTML = "Salmonoque"
+        // Salmonoque
+
+    } else if (DevmonPc == 2 ){
+        spanDevmonPc.innerHTML = "Dragonaitor"
+        // Dragonaitor
+
+    } else if (DevmonPc == 3 ){
+        spanDevmonPc.innerHTML = "Tortugor"
+        // Tortugor
+
+    } else if (DevmonPc == 4 ){
+        spanDevmonPc.innerHTML = "Tiburoncin"
+        // Tiburoncin
+
+    } else if (DevmonPc == 5 ){
+        spanDevmonPc.innerHTML = "Palmon"
+        // Palmon
+        
+    } else if (DevmonPc == 6 ){
+        spanDevmonPc.innerHTML = "Leñon"
+        // Leñon
+        
+    }
 }
 
+function ramdom(min, max) {
+    return Math.floor(Math.random() * (max-min+1)+min)
+}
 
 
 window.addEventListener("load",iniciarJuego)
